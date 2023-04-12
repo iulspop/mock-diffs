939322ac6a6be4c0a2741c9456a0b22522e9130b
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-04-12 11:29:31 -0400

    Quick commit

---
+++ codeql/java/stack-trace-exposure.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            String output = generateOutput(request);
+            response.getWriter().write(output);
+        } catch (Exception e) {
+            response.sendError(401, e.getMessage());
+        }
+    }
+
+    private String generateOutput(HttpServletRequest request) {
+        // Generate output based on the request
+        return "Sample output";
+    }
+}
