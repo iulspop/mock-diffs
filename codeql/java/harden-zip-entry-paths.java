a86bf7467791014fd5bd5dbdb240363968e2485b
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-04-12 11:57:51 -0400

    Quick commit

--- codeql/java/harden-zip-entry-paths.java
+++ codeql/java/harden-zip-entry-paths.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import io.github.pixee.security.ZipSecurity;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,7 +11,7 @@ import java.util.zip.ZipInputStream;
 public class UnzipManager {
 
     public void unzip(InputStream is, String unzipTargetDirectory) throws IOException {
-        ZipInputStream zip = new ZipInputStream(is);
+        ZipInputStream zip = ZipSecurity.createHardenedInputStream(is, StandardCharsets.UTF_8);
         ZipEntry zipEntry = zip.getNextEntry();
         while (zipEntry != null) {
             File file = new File(unzipTargetDirectory, zipEntry.getName());
