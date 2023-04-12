edcf27f9d07282d07cb850fc2c6154123621346a
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-04-12 10:42:14 -0400

    Quick commit

---
+++ codeql/java/upgrade-sslcontext-tls.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.IOException;
+
+public class SecureSocketManager {
+
+    public SSLSocket createSSLSocket(String host, int port) throws IOException {
+        SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket sslSocket = (SSLSocket) factory.createSocket(host, port);
+        sslSocket.setEnabledProtocols(new String[] { "TLSv1.1" });
+        
+        return sslSocket;
+    }
+    
+    // Other methods related to the SSL socket management
+}
