5b3813c54dcd6c3fe0784d25db44c36ef0bb04e8
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-04-12 11:49:41 -0400

    Quick commit

--- codeql/java/harden-java-deserialization.java
+++ codeql/java/harden-java-deserialization.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import io.github.pixee.security.ObjectInputFilters;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -9,6 +10,7 @@ public class DeserializationManager {
     public UserDetails deserializeFromFile(String filePath) throws IOException, ClassNotFoundException {
         try (FileInputStream fis = new FileInputStream(filePath);
              ObjectInputStream ois = new ObjectInputStream(fis)) {
+            ObjectInputFilters.enableObjectFilterIfUnprotected(ois);
             UserDetails userDetails = (UserDetails) ois.readObject();
             return userDetails;
         }
